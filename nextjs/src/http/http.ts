431540e4b51c58febebfaf9feae6bc2e0f4782f0
Gabriel Sena <stardusteight.d4cc@gmail.com>
2023-04-22 22:05:23 -0300

    initializing home page component

---
+++ nextjs/src/http/http.ts
@@ -0,0 +1,52 @@
+export default class ClientHttp {
+  static API_URL: string = 'http://localhost:3000/api' as string
+
+  static async get(path: string) {
+    const response = await fetch(`${ClientHttp.API_URL}/${path}`, {
+      headers: {
+        Authorization:
+          'Bearer eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..XnskWOEMEV-7teRh.Kfx1uSJGTBQzhMdy_gaDlCNz_Lz9UvSeKBCMBPHdKOZmN-WoZZRkOrFwmdbBDfKjRg7Y48VnP_9-K5xkUP4H6nGeCvc9ovRvuKlGFKm2ja_E3CjYjGLHRFLjrcYIu3qb9YPNYIZ41leztsY20HPbtQ7lyxUjKYwjDKvU3e59hvg8QNjc3dQyglaCvzs.TIHDzzl-FrxwwH3hKFr1Wg',
+      },
+    })
+    const data = await response.json()
+    if (!response.ok) throw new Error(data.message)
+    return data
+  }
+
+  static async post(path: string, body: any) {
+    const response = await fetch(`${ClientHttp.API_URL}/${path}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
+    const data = await response.json()
+    if (!response.ok) throw new Error(data.message)
+    return data
+  }
+
+  static async put(path: string, body: any) {
+    const response = await fetch(`${ClientHttp.API_URL}/${path}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
+    const data = await response.json()
+    if (!response.ok) throw new Error(data.message)
+    return data
+  }
+
+  static async delete(path: string) {
+    const response = await fetch(`${ClientHttp.API_URL}/${path}`, {
+      method: 'DELETE',
+    })
+    const data = await response.json()
+    if (!response.ok) throw new Error(data.message)
+    return data
+  }
+}
+
+export const fetcher = (path: string) => ClientHttp.get(path)
--- nextjs/tailwind.config.js
+++ nextjs/tailwind.config.js
@@ -7,10 +7,11 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        black: '#202123',
+        gray: '#343541',
+        text: '#d9d9e3',
+        'text-variant': '#ececf1',
       },
     },
   },
